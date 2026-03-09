e1da172ec79cb1bcc521bdae9e31e7388cc99409
Ben Tossell <12862117+bentosseII@users.noreply.github.com>
2026-03-09 01:24:14 -0400

    Add src/scenes/cookbook-templates/index.ts

---
+++ src/scenes/cookbook-templates/index.ts
@@ -0,0 +1,5 @@
+export {TerminalScene} from './TerminalScene';
+export {BrowserScene} from './BrowserScene';
+export {ConceptScene} from './ConceptScene';
+export {EditorScene} from './EditorScene';
+export * from './colors';
